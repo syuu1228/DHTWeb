8373abdd34a3415667c3f1acb989cc4ea381ef59
syuu1228 <syuu@dokukino.com>
2010-02-24 19:02:25 +0900

    add router

---
+++ src/org/dhtfox/DHTRouter.java
@@ -0,0 +1,16 @@
+package org.dhtfox;
+
+public class DHTRouter {
+
+	/**
+	 * @param args
+	 * @throws InterruptedException 
+	 */
+	public static void main(String[] args) throws InterruptedException {
+		// TODO Auto-generated method stub
+		DHTFox dht = new DHTFox("aaa", false);
+		while(true)
+			Thread.sleep(10000);
+	}
+
+}
